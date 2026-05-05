f0e0eaaa5b7e13a181d65a8e52738cc9ae7c42b5
RidusM <stormkillpeople@gmail.com>
2026-05-05 23:02:31 +0500

    fix: change callerSkip

--- logger/adapter_zap.go
+++ logger/adapter_zap.go
@@ -11,7 +11,7 @@ import (
 const (
 	// _argsPairs indicates that key-value arguments are expected in pairs.
 	_argsPairs  = 2
-	_callerSkip = 1
+	_callerSkip = 4
 )
 
 // ZapLogger holds a zap.Logger instance along with its sugared version and effective log level.
